70687c2137e31392fd01e52c0bfc2d731de156c9
glazunov Andrew <aglazunov@tomsk.lemz.ru>
2019-09-25 23:50:53 +0700

    fix debugger

--- IIP_labs/lab_1/src/Debugger/Debugger.java
+++ IIP_labs/lab_1/src/Debugger/Debugger.java
@@ -3,17 +3,16 @@ package Debugger;
 public class Debugger {
     private static boolean isEnabled = false;
 
-    public static void enableDebugger() {
-        isEnabled = true;
-    }
+    public static void enableDebugger() { isEnabled = true; }
 
-    public static void diseble
+    public static void disableDebugger() { isEnabled = false; }
 
-    public static void isEnabled() {
-
-    }
+    public static boolean isEnabled() { return isEnabled; }
 
     public static void log (Object obj) {
+        if (isEnabled)
+            return;
+
         System.out.println( obj.toString() );
     }
 }
