66444e64dbebcf9eabac33e409847ce5c6987fac
andrwnv <glazynovand@gmail.com>
2019-09-29 00:44:05 +0700

    add debugger

---
+++ IIP_labs/lab_3/Debugger/Debugger.java
@@ -0,0 +1,24 @@
+package Debugger;
+
+public class Debugger {
+    private static boolean isEnabled = false;
+
+    public static void enableDebugger() {
+        isEnabled = true;
+    }
+
+    public static void disableDebugger() {
+        isEnabled = false;
+    }
+
+    public static boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public static void log (Object obj) {
+        if (isEnabled)
+            return;
+
+        System.out.println( obj.toString() );
+    }
+}
