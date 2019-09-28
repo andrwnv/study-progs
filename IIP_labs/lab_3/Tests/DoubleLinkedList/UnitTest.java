d0e91382159b3c0f823a030ce240ca5faf8438eb
andrwnv <glazynovand@gmail.com>
2019-09-29 00:43:39 +0700

    add base for unit tests

---
+++ IIP_labs/lab_3/Tests/DoubleLinkedList/UnitTest.java
@@ -0,0 +1,12 @@
+package Tests.DoubleLinkedList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test_1(){
+        assertEquals(0, 0);
+    }
+}
