9671f8637c0f2048523a02156d7f564b56be51fa
glazunov Andrew <aglazunov@tomsk.lemz.ru>
2019-10-11 20:55:30 +0700

    OS lab3 DONE

---
+++ OS_labs/os_lab_3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define file_path "/tmp/saved_pid.txt"
+
+int main() {
+    int pid = 0;
+
+    FILE* file = fopen(file_path, "r");
+
+    if (!file) {
+        pid = fork(); // get child PID
+
+        if (pid == 0) {
+            while(1) {
+                sleep(1);
+            }
+
+        } else {
+            printf("Child process creadted! \n");
+            file = fopen(file_path, "w"); // save PID to tmp
+            fprintf(file, "%d", pid);
+            fclose(file);
+        }
+
+    } else {
+        fscanf(file, "%d", &pid);
+        fclose(file);
+        printf("Child process killed! \n");
+        kill(pid, 15); // Use SIGTERM for kill
+        remove(file_path);
+    }
+}
