78cff6bdcb06ad6c9b4d74a3666e7a0d0f07ad94
Глазунов Андрей Юрьевич <glazunov_ay@lemz-t.ru>
2020-03-06 13:17:32 +0700

    [ASM]: hello world

---
+++ spring semester 2 course/operatin_system_labs/lab_1s.asm
@@ -0,0 +1,23 @@
+masm
+model small 
+
+.data
+    message db 'Hello AMS$'
+
+.stack
+    db 256 dup ('?')
+
+.code
+    main proc
+        mov ax, @data
+        mov ds, ax
+        mov ah, 9
+        mov dx, offset message
+
+        int 21h
+        mov ax, 4c00h
+        int 21h
+
+    main endp
+
+end main
