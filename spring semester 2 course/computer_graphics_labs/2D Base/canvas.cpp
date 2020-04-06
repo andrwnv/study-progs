4c301e76c08fd2aac5e5ff923adb183ddebd300b
Andrew G <glazynovand@gmail.com>
2020-04-06 14:27:03 +0300

    add basic manip w/ figure

--- spring semester 2 course/computer_graphics_labs/2D Base/canvas.cpp
+++ spring semester 2 course/computer_graphics_labs/2D Base/canvas.cpp
@@ -1,5 +1,7 @@
 #include "canvas.h"
 
+#include <cmath>
+
 #include <QPolygon>
 #include <QPainter>
 #include <QVector>
@@ -7,6 +9,7 @@
 #include <QLayout>
 #include <QLabel>
 #include <QPoint>
+#include <QDebug>
 
 Canvas::Canvas(QWidget *parent) : QWidget(parent)
 {
@@ -16,6 +19,8 @@ Canvas::Canvas(QWidget *parent) : QWidget(parent)
     _pixmap = QPixmap(1000, 600);
     _pixmap.fill(0xF0F0F0);
 
+    painter = new QPainter(&_pixmap);
+
     layout()->addWidget(_label);
 
     _label->setPixmap(_pixmap);
@@ -23,8 +28,7 @@ Canvas::Canvas(QWidget *parent) : QWidget(parent)
 
 void Canvas::drawAxis()
 {
-    QPainter painter(&_pixmap);
-    painter.setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
+    painter->setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
 
     QPen pen(Qt::black, 2, Qt::SolidLine);
 
@@ -32,21 +36,21 @@ void Canvas::drawAxis()
     brush.setColor(Qt::black);
     brush.setStyle(Qt::SolidPattern);
 
-    painter.setPen(pen);
+    painter->setPen(pen);
 
     // Рисуем Оx.
-    painter.drawLine( 0, _pixmap.height() / 2,
+    painter->drawLine( 0, _pixmap.height() / 2,
                       _pixmap.width(), _pixmap.height() / 2 );
 
     // Рисуем Оy.
-    painter.drawLine( _pixmap.width() / 2, 0,
+    painter->drawLine( _pixmap.width() / 2, 0,
                       _pixmap.width() / 2, _pixmap.height() );
 
     // Рисуем единичные интервалы, на Ox и Oy соответственно.
-    painter.drawLine( _pixmap.width() / 2 + _padding, _pixmap.height() / 2 - 10,
+    painter->drawLine( _pixmap.width() / 2 + _padding, _pixmap.height() / 2 - 10,
                       _pixmap.width() / 2 + _padding, _pixmap.height() / 2 + 10 );
 
-    painter.drawLine( _pixmap.width() / 2 - 10, _pixmap.height() / 2 - _padding,
+    painter->drawLine( _pixmap.width() / 2 - 10, _pixmap.height() / 2 - _padding,
                       _pixmap.width() / 2 + 10, _pixmap.height() / 2 - _padding );
 
     // Треугольник - указатель на Оx.
@@ -57,8 +61,8 @@ void Canvas::drawAxis()
     QPolygon triangle(triagnleDots);
 
     // Рисуем указатель на Оx.
-    painter.setBrush(brush);
-    painter.drawPolygon(triangle);
+    painter->setBrush(brush);
+    painter->drawPolygon(triangle);
 
     // Треугольник - указатель на Оy.
     triagnleDots = { { _pixmap.width() / 2, 0     },
@@ -70,16 +74,19 @@ void Canvas::drawAxis()
     triangle.append(triagnleDots);
 
     // Рисуем указатель на Оx.
-    painter.setBrush(brush);
-    painter.drawPolygon(triangle);
+    painter->setBrush(brush);
+    painter->drawPolygon(triangle);
+
+    // Удаляем ксить из painer.
+    painter->setBrush(Qt::BrushStyle::NoBrush);
 
     // Подписываем оси и единичные интервалы.
-    painter.setFont( QFont("Arial", 12) );
-    painter.drawText( _pixmap.width() / 2 - 15, _pixmap.height() / 2 - 5 , "0" );
-    painter.drawText( _pixmap.width() / 2 + 35, _pixmap.height() / 2 - 5 , "1" );
-    painter.drawText( _pixmap.width() / 2 - 15, _pixmap.height() / 2 - 55, "1" );
-    painter.drawText( _pixmap.width() - 15    , _pixmap.height() / 2 - 5 , "Y" );
-    painter.drawText( _pixmap.width() / 2 + 5 , 15                       , "X" );
+    painter->setFont( QFont("Arial", 12) );
+    painter->drawText( _pixmap.width() / 2 - 15, _pixmap.height() / 2 - 5 , "0" );
+    painter->drawText( _pixmap.width() / 2 + 35, _pixmap.height() / 2 - 5 , "1" );
+    painter->drawText( _pixmap.width() / 2 - 15, _pixmap.height() / 2 - 55, "1" );
+    painter->drawText( _pixmap.width() - 15    , _pixmap.height() / 2 - 5 , "Y" );
+    painter->drawText( _pixmap.width() / 2 + 5 , 15                       , "X" );
 
     // Обновляем pixmap.
     _label->setPixmap(_pixmap);
@@ -87,11 +94,10 @@ void Canvas::drawAxis()
 
 void Canvas::drawFigure()
 {
-    QPainter painter(&_pixmap);
-    painter.setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
+    painter->setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
 
     QPen pen(Qt::red, 2, Qt::SolidLine);
-    painter.setPen(pen);
+    painter->setPen(pen);
 
     // Записываем точки многоугольника.
     _figureDots.clear();
@@ -106,12 +112,136 @@ void Canvas::drawFigure()
         { _pixmap.width() / 2 + _padding * 2, static_cast<int>(_pixmap.height() / 2 - _padding * 4.5) }
     };
 
+    _transformedFigureDots = _figureDots;
+
     // Рисуем многоугольник.
     QPolygon figure(_figureDots);
-    painter.drawPolygon(figure);
+    painter->drawPolygon(figure);
+
+    reflectFigure();
 
     // Обновляем pixmap.
     _label->setPixmap(_pixmap);
 }
+
+void Canvas::scaleFigure(double scaleIndex)
+{
+    painter->setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
+
+    QPen pen(Qt::blue, 2, Qt::SolidLine);
+    painter->setPen(pen);
+
+    QVector< QPair<double, double> > rotationMatrix = {
+        { scaleIndex, 0 },
+        { 0, scaleIndex }
+    };
+
+    for (auto & point : _transformedFigureDots)
+    {
+        auto tmpPoint = point;
+
+        point.setX( tmpPoint.x() * rotationMatrix.at(0).first + tmpPoint.y() * rotationMatrix.at(0).second );
+        point.setY( tmpPoint.x() * rotationMatrix.at(1).first + tmpPoint.y() * rotationMatrix.at(1).second );
+    }
+
+    QPolygon figure(_transformedFigureDots);
+    painter->drawPolygon(figure);
+
+    // Обновляем pixmap.
+    _label->setPixmap(_pixmap);
+}
+
+void Canvas::rotateFigure(double angle)
+{
+    painter->setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
+
+    QPen pen(Qt::blue, 2, Qt::SolidLine);
+    painter->setPen(pen);
+
+    QVector< QPair<double, double> > rotationMatrix = {
+        {  std::cos(angle * 3.14), std::sin(angle * 3.14) },
+        { -std::sin(angle * 3.14), std::cos(angle * 3.14) }
+    };
+
+    for (auto & point : _transformedFigureDots)
+    {
+        auto tmpPoint = point;
+
+        point.setX( tmpPoint.x() * rotationMatrix.at(0).first + tmpPoint.y() * rotationMatrix.at(0).second );
+        point.setY( tmpPoint.x() * rotationMatrix.at(1).first + tmpPoint.y() * rotationMatrix.at(1).second );
+    }
+
+    QPolygon figure(_transformedFigureDots);
+    painter->drawPolygon(figure);
+
+    // Обновляем pixmap.
+    _label->setPixmap(_pixmap);
+}
+
+void Canvas::reflectFigure()
+{
+    painter->setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
+
+    QPen pen(Qt::blue, 2, Qt::SolidLine);
+    painter->setPen(pen);
+
+    QVector< QPair<double, double> > reflectionMatrix = {
+        { -1, 0 },
+        {  0, 1 }
+    };
+
+    for (auto & point : _transformedFigureDots)
+    {
+        auto tmpPoint = point;
+
+        if (reflectionMatrix.at(0).first < 0)
+            point.setX( width() + reflectionMatrix.at(0).first * tmpPoint.x() );
+        else if (reflectionMatrix.at(0).second < 0)
+            point.setY( height() + reflectionMatrix.at(1).second * tmpPoint.y());
+    }
+
+    QPolygon figure(_transformedFigureDots);
+    painter->drawPolygon(figure);
+
+    // Обновляем pixmap.
+    _label->setPixmap(_pixmap);
+}
+
+void Canvas::transferFigure(int trasferX, int transferY)
+{
+    painter->setRenderHint(QPainter::Antialiasing, true); // Добавляем сглаживание для линии.
+
+    QPen pen(Qt::blue, 2, Qt::SolidLine);
+    painter->setPen(pen);
+
+    QVector< QPair<int, int> > transferMatrix = {
+        { trasferX * _padding, 0  },
+        { 0, transferY * _padding }
+    };
+
+    QVector<QPoint> figureDotesCopy = _figureDots;
+
+    for (auto & point : figureDotesCopy)
+    {
+        auto tmpPoint = point;
+
+        point.setX( tmpPoint.x() + transferMatrix.at(0).first );
+        point.setY( tmpPoint.y() + transferMatrix.at(1).second );
+    }
+
+    QPolygon figure(figureDotesCopy);
+    painter->drawPolygon(figure);
+
+    // Обновляем pixmap.
+    _label->setPixmap(_pixmap);
+}
+
+void Canvas::clearPixmap()
+{
+    _pixmap.fill(Qt::white);
+    _label->setPixmap(_pixmap);
+}
+
+void Canvas::clearFigureChanges() { _transformedFigureDots = _figureDots; }
 
 
--- spring semester 2 course/computer_graphics_labs/2D Base/canvas.h
+++ spring semester 2 course/computer_graphics_labs/2D Base/canvas.h
@@ -1,23 +1,35 @@
 #pragma once
 
 #include <QPaintEvent>
+#include <QPainter>
 #include <QWidget>
 #include <QLabel>
 
 class Canvas : public QWidget
 {
     Q_OBJECT
+
 public:
     explicit Canvas(QWidget *parent = nullptr);
     void drawAxis();
     void drawFigure();
 
+    void scaleFigure(double scaleIndex);
+    void rotateFigure(double angle);
+    void reflectFigure();
+    void transferFigure(int trasferX, int transferY);
+
+    void clearPixmap();
+    void clearFigureChanges();
+
 private:
+    QPainter* painter;
     QPixmap _pixmap;
     QLabel* _label;
 
     int             _padding = 50;
     QVector<QPoint> _figureDots;
+    QVector<QPoint> _transformedFigureDots;
 };
 
 
