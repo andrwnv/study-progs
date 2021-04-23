c9ddadda31968f22747739b68f6add18f7358a00
Andrew G <glazynovand@gmail.com>
2021-04-23 19:39:14 +0300

    lab 5

---
+++ spring semester 2 course/operatin_system_labs/lab_5/lab_5.asm
@@ -0,0 +1,45 @@
+data segment para public 'data'
+    arr dd 2, 5 , 2, 5, 4, 5, 6, 5, 6
+    arr_size dw 9
+data ends
+
+stk segment stack
+    db 256 dup ('?')
+stk ends
+
+print macro ax, dx
+    mov dl, 30h
+    add dl, al
+    mov ah, 2h
+    int 21h
+endm
+
+code segment para 'code'
+    assume cs:code, ds:data, ss:stk
+    
+    main proc
+        mov ax, data
+        mov ds, ax
+        
+        mov cx, arr_size
+        
+        mov si, offset arr 
+        
+        arr_loop:
+            mov ax, [si]
+            test al, 1
+            jnz if_odd
+            xor ax, ax
+            
+        if_odd:
+            print ax, dx
+            
+            add si, 4h
+            loop arr_loop
+        
+        prog_end:
+            mov ax, 4c00h
+            int 21h
+    main endp
+code ends
+end main
