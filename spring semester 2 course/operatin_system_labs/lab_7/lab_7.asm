98038c0aac99f7a967edeb9b17811fbf10e42401
Andrew G <glazynovand@gmail.com>
2021-05-14 20:07:24 +0700

    update lab 7

--- spring semester 2 course/operatin_system_labs/lab_7/lab_7.asm
+++ spring semester 2 course/operatin_system_labs/lab_7/lab_7.asm
@@ -1,6 +1,8 @@
 data segment para public 'data'
-    arr db 2, 5 , 2, 5, 4, 5, 6, 5, 6
-    arr_size=$-arr
+    arr db 1, 2, 3
+        db 5, 152, 7
+        db 9, 2, 13
+    nSize=3
 data ends
 
 stk segment stack
@@ -29,33 +31,92 @@ code segment para 'code'
     cld
     lea si, arr
     lea di, arr
-    mov cx, arr_size
     
-    arr_loop:
-        lodsb
+    mov ch, nSize
+    arr_loop_1:
+        mov cl, nSize
         
-        test al, 1
-        jnz if_odd
-        xor ax, ax
+        arr_loop_2:
+            lodsb
         
-        if_odd:
-            stosb
-            loop arr_loop
+            test al, 1
+            jnz if_odd
+            xor ax, ax
+
+            if_odd:
+                stosb
+                dec cl
+                jnz arr_loop_2
+        dec ch
+        jnz arr_loop_1
         
     exit:
         cld
         lea si, arr
-        mov cx, arr_size
-        
-        print_loop:
-            lodsb
-            print ax, dx
-            loop print_loop
+
+        mov ch, nSize
+        arr_loop_12:
+            mov cl, nSize
+
+            arr_loop_22:
+                mov ah, 2h
+                mov dl, 20h
+                int 21h
+                
+                lodsb
+                call print_decimal
+
+                dec cl
+                jnz arr_loop_22
+            
+            mov ah, 2h
+            mov dl, 13
+            int 21h
+            
+            mov dl, 10
+            int 21h
+                
+            dec ch
+            jnz arr_loop_12
             
         mov ax, 4c00h
         int 21h
      
     main endp
+    
+    print_decimal proc
+        assume cs:code, ds:data, ss:stk, es: data
+
+        push dx
+        push bx
+        push cx
+        
+        xor ah, ah
+        xor cx, cx
+        mov bx, 0ah
+        
+        output_dec:
+            xor dx, dx
+            div bx
+            push dx
+            inc cx
+            or ax, ax
+            jne output_dec
+        
+        mov ah, 2h
+        
+        disp:
+            pop dx
+            or dl, 30h
+            int 21h
+            loop disp
+        
+        pop cx
+        pop bx 
+        pop dx
+
+        ret
+    print_decimal endp
 code ends
 end main
 
